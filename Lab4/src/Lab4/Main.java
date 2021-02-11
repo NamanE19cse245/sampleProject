5bae80f34f97f0573315b78db3550693e9fbaf6d
Naman Mayer <namanmayer5@gmail.com>
2021-02-11 17:13:29 +0530

    added selection sort to NewSort:

--- Lab4/src/Lab4/Main.java
+++ Lab4/src/Lab4/Main.java
@@ -12,10 +12,20 @@ public class Main {
 	public static void main(String args[])
 	{
 		int[] array = {98,45,65,21,34,55,32,89,87,100};
+		
+		int[] array1 = array;
+		
 		bubbleSort b = new bubbleSort(array);
 		array = b.Sort();
+		
 		Main m = new Main();
 		m.printArray(array);
+		
+		System.out.println();
+		
+		selectionSort s = new selectionSort(array1);
+		array1 = s.Sort();
+		m.printArray(array1);
 	}
 
 }
--- Lab4/src/Lab4/selectionSort.java
+++ Lab4/src/Lab4/selectionSort.java
@@ -1,5 +1,35 @@
 package Lab4;
 
 class selectionSort {
-
+	
+	int[] array;
+	selectionSort(int[] array)
+	{
+		this.array = array;
+	}
+	
+	int[] Sort()
+	{
+		int smallest;
+		int position;
+		for(int i=0;i<array.length;i++)
+		{
+			smallest = array[i];
+			position = i;
+			for(int j=i;j<array.length;j++)
+			{
+				if(smallest>array[j])
+				{
+					smallest = array[j];
+					position = j;
+				}
+			}
+			int temp = array[i];
+			array[i] = array[position];
+			array[position] = temp;
+		}
+		
+		System.out.println("Selection sort complete");
+		return array;
+	}
 }
